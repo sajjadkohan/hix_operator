25b3b87b5e2ca84f2ba672758a4944326d2dd309
Ali Reza Delbari <Alirezadelbari20@yahoo.com>
2024-12-10 12:02:24 +0330

    delvar

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,8 +1,8 @@
 const themeColorLayer3 = 'ffe1cc';
-// const BASE_URL="http://localhost:3000";
-const BASE_URL="https://hixnew.liara.run";
-const CHAT_BASE_URL = "https://hixnew.liara.run";
-// const CHAT_BASE_URL = "http://localhost:3000";
+const BASE_URL="http://localhost:3000";
+// const BASE_URL="https://hixnew.liara.run";
+// const CHAT_BASE_URL = "https://hixnew.liara.run";
+const CHAT_BASE_URL = "http://localhost:3000";
 const BASEURLIMAGE = 'https://hix.storage.c2.liara.space/'
 
 export {
