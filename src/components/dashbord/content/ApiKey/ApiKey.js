e3f4559859d4c14a3a13c4c80c764aa09c238932
sajjadkohan <sajjad.k.9093@gmail.com>
2024-12-25 01:24:18 +0330

    code style and copy btn

--- src/components/dashbord/content/ApiKey/ApiKey.js
+++ src/components/dashbord/content/ApiKey/ApiKey.js
@@ -1,28 +1,39 @@
 import React, { useContext } from 'react'
 import styles from './ApiKey.module.css'
 import { AuthCtx } from '../../../../context/AuthContext'
+import CodeBlock from '../../../../global/CodeBlock/CodeBlock';
 const ApiKey = () => {
 
     const {user} = useContext(AuthCtx);
 // console.log(user);
 
   return (
+    <>
+    {
+       user&& user?.Roles.find(item => item === 'USER')?
     <div className={styles.apiKey}>
-        <div className={styles.showKey}>key : {user?.merchantId}</div>
+        key :<CodeBlock code={`${user.apiKey}`}/>
+        {/* <div className={styles.showKey}></div> */}
         <br/>
         <div className={styles.script}>
-            {
-                `
-                <script 
-                    id="HixChat" 
-                    src="https://hix.storage.c2.liara.space/files/script.js"
-                    data-key="${user?.merchantId}"
-                    >
-                </script>
-                `
-            }
+        <CodeBlock code={
+    `
+    <script 
+        id="HixChat" 
+        src="https://hix.storage.c2.liara.space/files/script.js"
+        data-key="${user.apiKey}"
+        >
+    </script>
+    `
+        } />
+            
         </div>
     </div>
+    :
+    <h1>شما به این بخش دسترسی ندارید</h1>
+
+    }
+    </>
   )
 }
 
